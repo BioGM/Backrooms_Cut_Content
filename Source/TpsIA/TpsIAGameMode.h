2fc0e8455a7be4e949b9b9644f0b30278a0f3f22
Romitti Maciel <93846061+BioGM@users.noreply.github.com>
2024-09-26 17:38:53 -0300

    Ojo
    dea

---
+++ Source/TpsIA/TpsIAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TpsIAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATpsIAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATpsIAGameMode();
+};
+
+
+
